b51150d43c5bd5c48a94c7954234e78fa79ad801
Heeran Kim <heerankim3@gmail.com>
2025-02-17 08:02:39 +1000

    [backend][fix] Add missing import for LoginSerializer in users/views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
